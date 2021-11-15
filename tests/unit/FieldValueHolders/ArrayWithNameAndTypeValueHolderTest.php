ce70ba7a6def6b8cdc51c48d2ae4b2c2355dc9df
George <g.kiknadze@luchki.ru>
2021-11-15 15:44:50 +0300

    tests

---
+++ tests/unit/FieldValueHolders/ArrayWithNameAndTypeValueHolderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Entities\CustomFields\ValueHolders\ArrayWithNameAndTypeValueHolder;
+use App\Exceptions\InvalidValueException;
+use Codeception\Test\Unit;
+
+class ArrayWithNameAndTypeValueHolderTest extends Unit
+{
+        public function testArrayWithNameAndTypeValueHolder() {
+                $value_holder = (new ArrayWithNameAndTypeValueHolder('EnumBundleElement'));
+                $some_text = 'some_text';
+                $value_holder->setValue($some_text);
+                $this->assertEquals($some_text, $value_holder->getValue());
+                $this->assertEquals(
+                        [
+                                'name' => $some_text,
+                                '$type' => 'EnumBundleElement'
+                        ],
+                        $value_holder->getValueForApi());
+
+        }
+
+        public function testBadValue() {
+                $value_holder = (new ArrayWithNameAndTypeValueHolder('EnumBundleElement'));
+                $this->expectException(InvalidValueException::class);
+                $not_text = ['arr', 'bbrr'];
+                $value_holder->setValue($not_text);
+
+        }
+}
